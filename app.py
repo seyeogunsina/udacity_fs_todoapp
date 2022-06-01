02c51db7569d8075cea3abeba71e7a6a6d0e45ed
seyeogunsina <adeseyeogunsina@gmail.com>
2022-06-01 14:35:50 +0100

    last update before crud update on lists

--- app.py
+++ app.py
@@ -71,6 +71,13 @@ def delete_todo(todo_id):
         db.session.close()
     return jsonify({'success': True})
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', 
+    lists = TodoList.query.all(),
+    active_list = TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', todos=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
